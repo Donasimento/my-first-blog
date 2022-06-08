50ff0068910221eb846fedc9d7dbe9efb746a917
Donasimento <jsoe290794@hotmail.com>
2022-06-08 16:35:18 +0200

    Lista de posts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3g!ytg1%(f#iwet4@g%rrpc&k#!x^ofraz0%6#(8k1o9=e+6k@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'donasimento.pythonanywhere.com']
 
 
 # Application definition
